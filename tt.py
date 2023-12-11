6dc4212c04ac7af297440045cf9f9594e6a883ae
leeaejin03 <152825273+leeaejin03@users.noreply.github.com>
2023-12-11 10:06:21 +0900

    Add files via upload

--- tt.py
+++ tt.py
@@ -1,3 +1,32 @@
+
+#기말고사 시험범위
+
+
+import matplotlib.pyplot as plt
+import streamlit as st
+
+fig, ax = plt.subplots()
+
+c1 = st.sidebar.radio('선의 색을 선택하시오', ['red', 'green', 'blue', 'orange'])
+l1 = st.sidebar.radio('선의 형태를 선택하시오', ['-', ':', '-.', '--'])
+m1 = st.sidebar.radio('마커의 형태를 선택하시오', ['o', '^', 's', 'p', 'h'])
+
+x = []
+y = []
+
+for i in range(-20, 21, 3):
+    x.append(i)
+    y.append(-2*i*i + 3*i + 5)
+
+
+plt.plot(x, y, color = c1, linestyle = l1, markerstyle = m1)
+
+st.pyplot(fig)
+
+
+
+
+
 # from tkinter import S
 # import streamlit as st
 
@@ -485,6 +514,9 @@
 # '''
 # st.markdown(multi)
 
+import sys
+sys.exit()
+
 import streamlit as st
 import matplotlib.pyplot as plt
 import numpy as np
@@ -537,3 +569,10 @@ plt.xlim([-15,15])
 plt.ylim([-2000,2000])
 plt.legend()
 st.pyplot(fig)
+
+
+
+
+
+
+
