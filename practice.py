53dd259c2c33138997478f560b285f4f90b4e043
leeaejin03 <152825273+leeaejin03@users.noreply.github.com>
2023-12-11 11:00:14 +0900

    Add files via upload

--- practice.py
+++ practice.py
@@ -1,20 +1,31 @@
 import matplotlib.pyplot as plt
 import streamlit as st
+import numpy as np
 
 fig, ax = plt.subplots()
 
-c1 = st.sidebar.radio('선의 색을 선택하시오', ['red', 'green', 'blue', 'orange'])
-l1 = st.sidebar.radio('선의 형태를 선택하시오', ['-', ':', '-.', '--'])
-m1 = st.sidebar.radio('마커의 형태를 선택하시오', ['o', '^', 's', 'p', 'h'])
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    c1 = st.radio('선의 색을 선택하시오', ['red', 'green', 'blue', 'orange'])
+with col2:
+    l1 = st.radio('선의 형태를 선택하시오', ['-', ':', '-.', '--'])
+with col3:
+    m1 = st.radio('마커의 형태를 선택하시오', ['o', '^', 's', 'p', 'h'])
+
 
 x = []
 y = []
+ysin = []
 
-for i in range(-20, 21, 3):
+for i in range(-20, 21, 5):
     x.append(i)
-    y.append(-2*i*i + 3*i + 5)
+    y.append(3*i*i - 5*i + 2)
+    ysin.append(1200*np.sin(i))
 
-
-plt.plot(x, y, color = c1, linestyle = l1, markerstyle = m1)
+plt.plot(x, y, 'go--', label = '2nd Equation')
+plt.plot(x, ysin, 'rs:', label = 'sin Function')
+plt.legend()
+plt.plot(x, y, color = c1, linestyle = l1, marker = m1)
 
 st.pyplot(fig)
